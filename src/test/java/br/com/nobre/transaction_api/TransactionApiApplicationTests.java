60432bb67a1da0aff7e7314aed510ea55a9d829a
BrenoHenrique07 <brenohenrique.developer@gmail.com>
2025-03-17 19:32:33 -0300

    Inital commit

---
+++ src/test/java/br/com/nobre/transaction_api/TransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nobre.transaction_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
